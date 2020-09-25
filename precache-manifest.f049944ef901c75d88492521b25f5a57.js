5644252a438df43a0ff34df92d32a04ea48660e0
Pietro Ribeiro <ribpietro@gmail.com>
2020-09-24 22:02:19 -0300

    initial commit

---
+++ precache-manifest.f049944ef901c75d88492521b25f5a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac459cc81590f3e5c5072e1820a18b50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69c741ff91e725cde16",
+    "url": "/static/css/main.e95f7548.chunk.css"
+  },
+  {
+    "revision": "15d3d840a61e63ecedbe",
+    "url": "/static/js/2.88332d1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88332d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e69c741ff91e725cde16",
+    "url": "/static/js/main.121d29bb.chunk.js"
+  },
+  {
+    "revision": "c78ca65abae54dc6736f",
+    "url": "/static/js/runtime-main.b6f15b26.js"
+  }
+]);
